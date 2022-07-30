d3e48b638357d2453bd55b212a9134a21b4ca20c
Таня Глубокая <t.glubokaia@gmail.com>
2022-07-30 23:36:17 +0300

    Инициализирует проект

---
+++ src/components/main-screen/main-screen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function MainScreen() {
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      
+    </React.Fragment>
+
+  )
+}
+
+export default MainScreen;
